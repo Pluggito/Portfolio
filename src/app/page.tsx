bed444a6edda0e55b689cc656edce92b5905a6ed
Pluggito <Pluggito@example.com>
2025-04-28 22:50:01 +0100

    fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
 import Image from "next/image";
 import Home from "../components/home";
 import About from "../components/about";
-import Skills from "../components/skills";
 import Contact from "../components/contact";
 import Features from "../components/features";
+import SkillsTimeline from "../components/skills";
 
 export default function Page() {
   return (
     <div className="overflow-x-hidden">
       <Home />
       <About />
-      <Skills />
+      <SkillsTimeline/>
       <Features />
       <Contact />
     </div>
